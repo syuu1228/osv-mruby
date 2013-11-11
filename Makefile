34cf359fa1cc7eb59ba179bc2d0670d412399239
Takuya ASADA <syuu@dokukino.com>
2013-11-11 04:22:34 -0800

    import libuv

--- Makefile
+++ Makefile
@@ -5,13 +5,16 @@ module: mruby.so mirb.so bootfs.manifest usr.manifest
 libyaml = libyaml/api.o libyaml/reader.o libyaml/scanner.o libyaml/parser.o libyaml/_loader.o libyaml/writer.o libyaml/emitter.o libyaml/dumper.o
 libyaml-flags = -Wno-unused-value -DYAML_VERSION_STRING=\"0.1.4\" -DYAML_VERSION_MAJOR=0 -DYAML_VERSION_MINOR=1 -DYAML_VERSION_PATCH=4
 
-mruby-objects = mruby.o libmruby.a libffi_pic.a $(libyaml)
-mirb-objects = mirb.o libmruby.a libffi_pic.a $(libyaml)
+libuv = libuv/fs-poll.o libuv/inet.o libuv/uv-common.o libuv/version.o libuv/unix/async.o libuv/unix/core.o libuv/unix/dl.o libuv/unix/fs.o libuv/unix/getaddrinfo.o libuv/unix/loop-watcher.o libuv/unix/loop.o libuv/unix/pipe.o libuv/unix/poll.o libuv/unix/process.o libuv/unix/signal.o libuv/unix/stream.o libuv/unix/tcp.o libuv/unix/thread.o libuv/unix/threadpool.o libuv/unix/timer.o libuv/unix/tty.o libuv/unix/udp.o libuv/unix/linux-core.o libuv/unix/linux-inotify.o libuv/unix/linux-syscalls.o libuv/unix/proctitle.o
+
+mruby-objects = mruby.o libmruby.a libffi_pic.a $(libyaml) $(libuv)
+mirb-objects = mirb.o libmruby.a libffi_pic.a $(libyaml) $(libuv)
 
 define mruby-includes
   build/$(mode)/module/osv-mruby/mruby/src
   build/$(mode)/module/osv-mruby/mruby/include
   build/$(mode)/module/osv-mruby/include
+  build/$(mode)/module/osv-mruby/libuv
 endef
 
 cflags-mruby-include = $(foreach path, $(strip $(mruby-includes)), -isystem $(src)/$(path))
--- build_config.rb
+++ build_config.rb
@@ -74,15 +74,15 @@ MRuby::Build.new do |conf|
 #  conf.gem :git => 'https://github.com/iij/mruby-syslog.git'
   conf.gem :git => 'https://github.com/iij/mruby-tempfile.git'
 
-  conf.gem :git => 'https://github.com/mattn/mruby-http.git'
-
   conf.gem :git => 'https://github.com/suzukaze/mruby-msgpack.git'
 
+  conf.gem :git => 'https://github.com/mattn/mruby-uv.git'
+  conf.gem :git => 'https://github.com/mattn/mruby-http.git'
+#  conf.gem :git => 'https://github.com/mattn/mruby-sinatic.git'
+  conf.gem :git => 'https://github.com/mattn/mruby-json.git'
   conf.gem :git => 'https://github.com/mattn/mruby-msgpack-rpc.git'
 
   conf.gem :git => 'https://github.com/syuu1228/osv-mruby-polarssl.git'
-
-  conf.gem :git => 'https://github.com/mattn/mruby-json.git'
 
   conf.gem :git => 'https://github.com/AndrewBelt/mruby-yaml.git'
 
@@ -96,15 +96,9 @@ MRuby::Build.new do |conf|
 
   conf.gem :github => 'mobiruby/mruby-cfunc', :branch => 'master'
 
-  # Generate mirb command
-  conf.gem :core => "mruby-bin-mirb"
-
-  # Generate mruby command
-  conf.gem :core => "mruby-bin-mruby"
-
   # C compiler settings
   conf.cc do |cc|
-      cc.flags << "-fPIC -std=gnu99 -MD -MP -g -Wall -Wno-pointer-arith -Wformat=0 -D __BSD_VISIBLE=1 -U _FORTIFY_SOURCE -fno-stack-protector  -I. -I../../../../../arch/x64 -I../../../../.. -I../../../../../external/libunwind/include -I../../../../../include -isystem ../../../../../include/glibc-compat -isystem ../../../../../external/gcc.bin/usr/include/c++/4.8.1 -isystem ../../../../../external/gcc.bin/usr/include/c++/4.8.1/x86_64-redhat-linux -isystem ../../../../../external/acpica/source/include -isystem ../../../../../external/misc.bin/usr/include -isystem ../../../../../include/api -isystem ../../../../../include/api/x86_64 -isystem ../../../../../external/gcc.bin/usr/lib/gcc/x86_64-redhat-linux/4.8.1/include -isystem ../../../../../build/release/gen/include -isystem ../../../../../bsd/sys -isystem ../../../../../bsd/ -isystem ../../../../../bsd/sys -isystem ../../../../../bsd/  -D_KERNEL -fno-omit-frame-pointer -DHAVE_ATTR_COLD_LABEL -include ../../../../../compiler/include/intrinsics.hh  -isystem ../../../../../external/openjdk.bin/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.25-2.3.12.3.fc19.x86_64/include  -isystem ../../../../../external/openjdk.bin/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.25-2.3.12.3.fc19.x86_64/include/linux -msse4.1 -O2 -DACPI_MACHINE_WIDTH=64 -DACPI_USE_LOCAL_CACHE   -DCONF_preempt=1  -DCONF_debug_memory=0  -DCONF_logger_debug=0 -nostdinc -D__OSV__ -D__XEN_INTERFACE_VERSION__='0x00030207' -DXEN -DXENHVM -DSMP -D'__FBSDID(__str__)=extern int __bogus__' -D__x86_64__ -I ../../../../../libc/internal -I  ../../../../../libc/arch/x64 -Wno-missing-braces -Wno-parentheses -Wno-unused-but-set-variable -I../include"
+      cc.flags << "-fPIC -std=gnu99 -MD -MP -g -Wall -Wno-implicit-function-declaration -Wno-pointer-arith -Wformat=0 -D __BSD_VISIBLE=1 -U _FORTIFY_SOURCE -fno-stack-protector  -I. -I../../../../../arch/x64 -I../../../../.. -I../../../../../external/libunwind/include -I../../../../../include -isystem ../../../../../include/glibc-compat -isystem ../../../../../external/gcc.bin/usr/include/c++/4.8.1 -isystem ../../../../../external/gcc.bin/usr/include/c++/4.8.1/x86_64-redhat-linux -isystem ../../../../../external/acpica/source/include -isystem ../../../../../external/misc.bin/usr/include -isystem ../../../../../include/api -isystem ../../../../../include/api/x86_64 -isystem ../../../../../external/gcc.bin/usr/lib/gcc/x86_64-redhat-linux/4.8.1/include -isystem ../../../../../build/release/gen/include -isystem ../../../../../bsd/sys -isystem ../../../../../bsd/ -isystem ../../../../../bsd/sys -isystem ../../../../../bsd/  -D_KERNEL -fno-omit-frame-pointer -DHAVE_ATTR_COLD_LABEL -include ../../../../../compiler/include/intrinsics.hh  -isystem ../../../../../external/openjdk.bin/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.25-2.3.12.3.fc19.x86_64/include  -isystem ../../../../../external/openjdk.bin/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.25-2.3.12.3.fc19.x86_64/include/linux -msse4.1 -O2 -DACPI_MACHINE_WIDTH=64 -DACPI_USE_LOCAL_CACHE   -DCONF_preempt=1  -DCONF_debug_memory=0  -DCONF_logger_debug=0 -nostdinc -D__OSV__ -D__XEN_INTERFACE_VERSION__='0x00030207' -DXEN -DXENHVM -DSMP -D'__FBSDID(__str__)=extern int __bogus__' -D__x86_64__ -I ../../../../../libc/internal -I  ../../../../../libc/arch/x64 -Wno-missing-braces -Wno-parentheses -Wno-unused-but-set-variable -I../include"
     if ENV['BUILD_BIT'] == "64"
       cc.flags << ' -DMRB_INT64'
     end
