72376fb7091d916699d5b8d098c66ebc71622966
Takuya ASADA <syuu@dokukino.com>
2013-12-15 17:27:38 -0800

    sync with master

--- build_config.rb
+++ build_config.rb
@@ -74,7 +74,7 @@ MRuby::Build.new do |conf|
   conf.gem :git => 'https://github.com/iij/mruby-regexp-pcre.git'
   conf.gem :git => 'https://github.com/iij/mruby-require.git'
   conf.gem :git => 'https://github.com/iij/mruby-simple-random.git'
-  conf.gem :git => 'https://github.com/syuu1228/osv-mruby-socket.git'
+  conf.gem :git => 'https://github.com/iij/mruby-socket.git'
 #  conf.gem :git => 'https://github.com/iij/mruby-syslog.git'
   conf.gem :git => 'https://github.com/iij/mruby-tempfile.git'
 
@@ -86,17 +86,10 @@ MRuby::Build.new do |conf|
   conf.gem :git => 'https://github.com/mattn/mruby-json.git'
 #  conf.gem :git => 'https://github.com/mattn/mruby-msgpack-rpc.git'
 
-# This gem is licensed under GPL, disabled by default
-  conf.gem :git => 'https://github.com/syuu1228/osv-mruby-polarssl.git'
-
   conf.gem :git => 'https://github.com/AndrewBelt/mruby-yaml.git'
 
   conf.gem :git => 'https://github.com/syuu1228/osv-mruby-base64.git'
-# This gem is licensed under GPL, disabled by default
-  conf.gem :git => 'https://github.com/syuu1228/osv-mruby-sha1.git'
   conf.gem :git => 'https://github.com/syuu1228/osv-mruby-md5.git'
-
-  conf.gem :git => 'https://github.com/syuu1228/osv-mruby-vedis.git'
 
   conf.gem :git => 'https://github.com/matsumoto-r/mruby-sleep.git'
 
@@ -104,22 +97,16 @@ MRuby::Build.new do |conf|
 #  conf.gem :git => 'https://github.com/matsumoto-r/mruby-httprequest.git'
 #  conf.gem :git => 'https://github.com/iij/mruby-aws-s3.git'
 
-  conf.gem :github => 'mobiruby/mruby-cfunc', :branch => 'master'
+#  conf.gem :github => 'mobiruby/mruby-cfunc', :branch => 'master'
 
   conf.gem :git => 'https://github.com/syuu1228/mruby-eshell.git'
 
   # C compiler settings
   conf.cc do |cc|
-      cc.flags << "-fPIC -std=gnu99 -MD -MP -g -Wall -Wno-implicit-function-declaration -Wno-pointer-arith -Wformat=0 -D __BSD_VISIBLE=1 -U _FORTIFY_SOURCE -fno-stack-protector  -I. -I../../../../../arch/x64 -I../../../../.. -I../../../../../external/libunwind/include -I../../../../../include -isystem ../../../../../include/glibc-compat -isystem ../../../../../external/gcc.bin/usr/include/c++/4.8.1 -isystem ../../../../../external/gcc.bin/usr/include/c++/4.8.1/x86_64-redhat-linux -isystem ../../../../../external/acpica/source/include -isystem ../../../../../external/misc.bin/usr/include -isystem ../../../../../include/api -isystem ../../../../../include/api/x86_64 -isystem ../../../../../external/gcc.bin/usr/lib/gcc/x86_64-redhat-linux/4.8.1/include -isystem ../../../../../build/release/gen/include -isystem ../../../../../bsd/sys -isystem ../../../../../bsd/ -isystem ../../../../../bsd/sys -isystem ../../../../../bsd/  -D_KERNEL -fno-omit-frame-pointer -DHAVE_ATTR_COLD_LABEL -include ../../../../../compiler/include/intrinsics.hh  -isystem ../../../../../external/openjdk.bin/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.25-2.3.12.3.fc19.x86_64/include  -isystem ../../../../../external/openjdk.bin/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.25-2.3.12.3.fc19.x86_64/include/linux -msse4.1 -O2 -DACPI_MACHINE_WIDTH=64 -DACPI_USE_LOCAL_CACHE   -DCONF_preempt=1  -DCONF_debug_memory=0  -DCONF_logger_debug=0 -nostdinc -D__OSV__ -D__XEN_INTERFACE_VERSION__='0x00030207' -DXEN -DXENHVM -DSMP -D'__FBSDID(__str__)=extern int __bogus__' -D__x86_64__ -I ../../../../../libc/internal -I  ../../../../../libc/arch/x64 -Wno-missing-braces -Wno-parentheses -Wno-unused-but-set-variable -I../include"
-    if ENV['BUILD_BIT'] == "64"
-      cc.flags << ' -DMRB_INT64'
-    end
+      cc.flags << "-O0 -fPIC -Wall"
   end
 
-  # Linker settings
-  conf.linker do |linker|
-    if ENV['BUILD_BIT'] == "64"
-      linker.flags = '-DMRB_INT64'
-    end
-  end
+  conf.gem :git => 'https://github.com/syuu1228/osv-mruby-polarssl.git'
+  conf.gem :git => 'https://github.com/syuu1228/osv-mruby-sha1.git'
+  conf.gem :git => 'https://github.com/syuu1228/osv-mruby-vedis.git'
 end
