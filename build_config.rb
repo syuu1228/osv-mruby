1fc7d19fba059bb51f3d43cbab252a0ca5196ba8
Takuya ASADA <syuu@dokukino.com>
2013-12-15 20:37:53 -0800

    uncomment out networking packages

--- build_config.rb
+++ build_config.rb
@@ -78,13 +78,13 @@ MRuby::Build.new do |conf|
 #  conf.gem :git => 'https://github.com/iij/mruby-syslog.git'
   conf.gem :git => 'https://github.com/iij/mruby-tempfile.git'
 
-#  conf.gem :git => 'https://github.com/suzukaze/mruby-msgpack.git'
+  conf.gem :git => 'https://github.com/suzukaze/mruby-msgpack.git'
 
 #  conf.gem :git => 'https://github.com/mattn/mruby-uv.git'
 #  conf.gem :git => 'https://github.com/mattn/mruby-sinatic.git'
   conf.gem :git => 'https://github.com/mattn/mruby-http.git'
   conf.gem :git => 'https://github.com/mattn/mruby-json.git'
-#  conf.gem :git => 'https://github.com/mattn/mruby-msgpack-rpc.git'
+  conf.gem :git => 'https://github.com/mattn/mruby-msgpack-rpc.git'
 
   conf.gem :git => 'https://github.com/AndrewBelt/mruby-yaml.git'
 
@@ -93,9 +93,9 @@ MRuby::Build.new do |conf|
 
   conf.gem :git => 'https://github.com/matsumoto-r/mruby-sleep.git'
 
-#  conf.gem :git => 'https://github.com/matsumoto-r/mruby-simplehttp.git'
-#  conf.gem :git => 'https://github.com/matsumoto-r/mruby-httprequest.git'
-#  conf.gem :git => 'https://github.com/iij/mruby-aws-s3.git'
+  conf.gem :git => 'https://github.com/matsumoto-r/mruby-simplehttp.git'
+  conf.gem :git => 'https://github.com/matsumoto-r/mruby-httprequest.git'
+  conf.gem :git => 'https://github.com/iij/mruby-aws-s3.git'
 
 #  conf.gem :github => 'mobiruby/mruby-cfunc', :branch => 'master'
 
